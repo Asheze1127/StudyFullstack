008a04d3dd37e76ed6766a7090b434d438b2fee8
Asheze1127 <m.k.h11272005@gmail.com>
2025-12-25 01:03:41 +0900

    wip: 🚧  ログイン機能実装中

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
-
+"use client";
+import { Auth } from "../component/auth/auth";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans dark:bg-black">
-      <main className="w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  return (
+    <div className="font-sans">
+      <main className="bg-white">
+        <div className="max-w-2xl mx-auto py-32 px-16">
+          <Auth 
+          title="ログイン" 
+          description="ログインしてください" 
+          username={username} 
+          password={password} 
+          setUsername={setUsername} 
+          setPassword={setPassword} 
+          />
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/component/auth/auth.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import PersonIcon from '@mui/icons-material/Person';
+import PasswordIcon from '@mui/icons-material/Password';
+
+interface AuthProps {
+    title: string;
+    description: string;
+    username: string;
+    password: string;
+    setUsername: (username: string) => void;
+    setPassword: (password: string) => void;
+}
+
+export const Auth = ({ title, description, username, password, setUsername, setPassword }: AuthProps) => {
+    return (
+        <div>
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    {title}
+                </CardTitle>
+                <CardDescription>
+                    {description}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                    <form action="/login" method="post">
+                        <div className="flex flex-col gap-4 py-4">
+                            <PersonIcon className="margin-top-2"/>
+                            <Input type="email" placeholder="Email" name="email" value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <PasswordIcon className="margin-top-2" />
+                            <Input type="password" placeholder="Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="flex justify-center items-center gap-4 py-4">
+                            <Button className="w-1/2" type="submit">ログイン</Button>
+                            <Link href="/signup" className="w-1/2 text-center border border-gray-300 rounded-md p-2">新規登録</Link>
+                        </div>
+                    </form>
+                </CardContent>
+        </Card>
+        </div>
+    );
+}
